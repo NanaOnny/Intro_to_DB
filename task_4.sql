fa59b0f710cf919df37bc0defef0772d6c15079d
nana <nanaonny@outlook.com>
2024-07-28 23:08:21 +0000

    all changes

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME AS 'Column Name', 
+    COLUMN_TYPE AS 'Column Type', 
+    IS_NULLABLE AS 'Is Nullable', 
+    COLUMN_KEY AS 'Column Key', 
+    COLUMN_DEFAULT AS 'Default Value', 
+    EXTRA AS 'Extra Info'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
